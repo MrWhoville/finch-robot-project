925e73f50f4b019a8e31f434433fa551da47973d
C.H <christopherhernandez52909@gmail.com>
2025-06-02 13:54:31 -0700

    r

---
+++ src/main/java/com/whattheflip/example.java
@@ -0,0 +1,12 @@
+package com.svh#f;
+
+public class sis {
+    public static void main(String[] args){
+        Finch bird = new Finch();
+
+
+bird.setMove(direction: "F", distance:20. speed:100)
+        bird.stop();
+        bird.disconnect();
+    }
+}
