a5a22e012bcf15b5dd2cbbeee10dbee4f395843a
C.H <christopherhernandez52909@gmail.com>
2025-06-02 13:58:19 -0700

    Update example.java

--- src/main/java/com/whattheflip/example.java
+++ src/main/java/com/whattheflip/example.java
@@ -1,11 +1,13 @@
-package com.svh#f;
+package com.whattheflip;
 
-public class sis {
+import com.birdbrain.Finch;
+
+public class example {
     public static void main(String[] args){
         Finch bird = new Finch();
 
 
-bird.setMove(direction: "F", distance:20. speed:100)
+bird.setMove(direction: "F", distance:20. speed:100);
         bird.stop();
         bird.disconnect();
     }
